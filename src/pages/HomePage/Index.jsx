02d8310d43308244f983cadf968b502165ed4df5
Vinicius Alves <moroni.vinicius@hotmail.com>
2023-10-17 22:32:02 -0300

    removed icons from homepage

--- src/pages/HomePage/Index.jsx
+++ src/pages/HomePage/Index.jsx
@@ -1,17 +1,18 @@
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/auth";
 import { Container } from "react-bootstrap";
-import icons from "../../components/HomePageIcon/IconList.js";
-import HomePageIcon from "../../components/HomePageIcon/Index.jsx";
+// import icons from "../../components/HomePageIcon/IconList.js";
+// import HomePageIcon from "../../components/HomePageIcon/Index.jsx";
 
 const HomePage = () => {
   const { user } = useContext(AuthContext);
 
   return (
     <Container className="my-4 d-flex">
-      {icons.map((item) => (
+      {/* {icons.map((item) => (
         <HomePageIcon key={item.name} {...item} />
-      ))}
+      ))} */}
+      <p>Bem vindo, {user.email}!</p>
     </Container>
   );
 };
