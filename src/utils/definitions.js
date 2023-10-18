91118a55a90d9ae47564786b52fbe06f6e145158
Vinicius Alves <moroni.vinicius@hotmail.com>
2023-10-17 22:27:12 -0300

    urlBase updated

--- src/utils/definitions.js
+++ src/utils/definitions.js
@@ -1,2 +1,2 @@
-// export const urlBase = "https://129.146.68.51/aluno42-pfsii";
-export const urlBase = "http://localhost:4042";
+export const urlBase = "https://129.146.68.51/aluno42-pfsii";
+//export const urlBase = "http://localhost:4042";
